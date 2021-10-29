bf84f24bcb0c7fdc44e42ee1610f713ebe9431b7
Giuseppe Fusco <GIUSEPPE.FUSCO@local.ittcesena.it>
2021-10-29 09:37:58 +0200

    CLASSE BRANO FINITA

--- Brano.cs
+++ Brano.cs
@@ -6,13 +6,64 @@ namespace EsercitazioneGit
 {
     class Brano
     {
-        //string titolo
-        //string autore
-        //int durata
-
-        //Brano(titolo,autore,durata) { set/get};
-        //ToString()
-        //public bool ShortSong(int durata, Brano b);
+        private string _titolo;
+        private string _autore;
+        private int _durata;
+        public string Titolo
+        {
+            get { return _titolo; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("errore nell'inserimento del titolo");
+                }
+                _titolo = value;
+            }
+        }
+        public string Autore
+        {
+            get { return _autore; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("errore nell'inserimento dell'autore");
+                }
+                _autore = value;
+            }
+        }
+        public int Durata
+        {
+            get { return _durata; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("errore nell'inserimento della durata");
+                }
+                _durata = value;
+            }
+        }
+        public Brano(string titolo,string autore,int durata)
+        {
+            Titolo = titolo;
+            Autore = autore;
+            Durata = durata;
+        }
+        public override string ToString()
+        {
+            return "Titolo: "+Titolo+"\rAutore: "+Autore+"\rDurata(in secondi): "+Durata;
+        }
+        public bool ShortSong(int durata, Brano b)
+        {
+            bool soluzione=false;
+            if (b.Durata<durata)
+            {
+                soluzione = true;
+            }
+            return soluzione;
+        }
 
     }
 }
